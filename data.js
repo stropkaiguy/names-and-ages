8901b5fef7f636e67946aa9496aa145fb45f0a2d
Nick Carothers <93961846+stropkaiguy@users.noreply.github.com>
2022-02-25 16:27:27 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+export default [
+  
+  {
+    id: 1,
+    name: 'Anna Carothers',
+    age: 40,
+    image: 'https://scontent-atl3-2.xx.fbcdn.net/v/t31.18172-8/15384485_10154498955058941_3790905584256390802_o.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_ohc=-h9NMwShBFwAX9_B7hO&_nc_ht=scontent-atl3-2.xx&oh=00_AT8t7JaNaGc_6BLF1V0pQL9PpbBZYqU4iW_9Zm9ryLPBtQ&oe=624040C7',
+  },
+  {
+    id: 2,
+    name: 'Nick Carothers',
+    age: 36,
+    image: 'https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/87006269_10157672316453941_5813504901065474048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=730e14&_nc_ohc=Z8bpwJE2HP0AX-zMX7U&tn=IsgnU_gBfmvepihh&_nc_ht=scontent-atl3-2.xx&oh=00_AT_1KDE_TvKosAEq4CAmaJjInOifyceGWcd7l3nDFknATw&oe=623F87E9',
+  },
+  {
+    id: 3,
+    name: 'Genevieve Carothers',
+    age: 5,
+    image: 'https://scontent-atl3-2.xx.fbcdn.net/v/t39.30808-6/271547925_10159355653678941_2564761511816595417_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=IFDL69MIKywAX-6c5Ze&_nc_ht=scontent-atl3-2.xx&oh=00_AT-coQAz775ZoAVHxY35pbQTNgTTfsMiWfHhDvG0i5uthw&oe=621EB688',
+  },
+  {
+    id: 4,
+    name: 'Katarina',
+    age: 13,
+    image: 'https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/180627112_10158886933958941_6121021576054333716_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=lkw2NlwD-aUAX81vvMi&_nc_ht=scontent-atl3-2.xx&oh=00_AT_CofRHrQym38CHJk9NvVZaHWzavGfIfaZY0F5yEdjkew&oe=623D8C1D',
+  },
+  {
+    id: 5,
+    name: 'Penelope',
+    age: 11,
+    image: 'https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/169465087_10158835905713941_1590500524912397212_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_ohc=y2bDaOYL72wAX9jY8K3&_nc_ht=scontent-atl3-2.xx&oh=00_AT8tCCXAm7gwwXmCNl5p6oOKblatQ3f5hwxdBVGo6Zlhzw&oe=623DEB10',
+  }
+];
